4829e35d715358995ac3439021c348d39f1605d8
Raining <127360832+Raining9966@users.noreply.github.com>
2024-03-24 16:56:48 +0800

    Update StackImplementation.java

--- 02-1-stack/src/main/java/oy/tol/tra/StackImplementation.java
+++ 02-1-stack/src/main/java/oy/tol/tra/StackImplementation.java
@@ -1,95 +1,90 @@
 package oy.tol.tra;
 
-/**
- * An implementation of the StackInterface.
- * <p>
- * TODO: Students, implement this so that the tests pass.
- * 
- * Note that you need to implement construtor(s) for your concrete StackImplementation, which
- * allocates the internal Object array for the Stack:
- * - a default constructor, calling the StackImplementation(int size) with value of 10.
- * - StackImplementation(int size), which allocates an array of Object's with size.
- *  -- remember to maintain the capacity and/or currentIndex when the stack is manipulated.
- */
 public class StackImplementation<E> implements StackInterface<E> {
 
-   private Object [] itemArray;
-   private int capacity;
-   private int currentIndex = -1;
-   private static final int DEFAULT_STACK_SIZE = 10;
+    private E[] itemArray;
+    private int capacity;
+    private int currentIndex = -1;
+    private static final int DEFAULT_STACK_SIZE = 10;
 
-   /**
-    * Allocates a stack with a default capacity.
-    * @throws StackAllocationException
-    */
-   public StackImplementation() throws StackAllocationException {
-      // TODO: call the constructor with size parameter with default size of 10.
-      
-   }
+    public StackImplementation() throws StackAllocationException {
+        this(DEFAULT_STACK_SIZE);
+    }
 
-   /** TODO: Implement so that
-    * - if the size is less than 2, throw StackAllocationException
-    * - if the allocation of the array throws with Java exception,
-    *   throw StackAllocationException.
-    * @param capacity The capacity of the stack.
-    * @throws StackAllocationException If cannot allocate room for the internal array.
-    */
-   public StackImplementation(int capacity) throws StackAllocationException {
-      
-   }
+    @SuppressWarnings("unchecked")
+    public StackImplementation(int capacity) throws StackAllocationException {
+        if (capacity < 2) {
+            throw new StackAllocationException("Capacity must be at least 2.");
+        }
+        this.capacity = capacity;
+        this.itemArray = (E[]) new Object[capacity];
+    }
 
-   @Override
-   public int capacity() {
-      // TODO: Implement this
-      
-   }
+    @Override
+    public int capacity() {
+        return capacity;
+    }
 
-   @Override
-   public void push(E element) throws StackAllocationException, NullPointerException {
-      // TODO: Implement this
-               
-   }
+    @Override
+    public void push(E element) throws StackAllocationException, NullPointerException {
+        ensureCapacity();
+        if (element == null) {
+            throw new NullPointerException("Cannot push null into the stack.");
+        }
+        itemArray[++currentIndex] = element;
+    }
 
-   @SuppressWarnings("unchecked")
-   @Override
-   public E pop() throws StackIsEmptyException {
-      
-   }
+    @Override
+    public E pop() throws StackIsEmptyException {
+        if (isEmpty()) {
+            throw new StackIsEmptyException("Cannot pop from an empty stack.");
+        }
+        return itemArray[currentIndex--];
+    }
 
-   @SuppressWarnings("unchecked")
-   @Override
-   public E peek() throws StackIsEmptyException {
-      
-   }
+    @Override
+    public E peek() throws StackIsEmptyException {
+        if (isEmpty()) {
+            throw new StackIsEmptyException("Cannot peek into an empty stack.");
+        }
+        return itemArray[currentIndex];
+    }
 
-   @Override
-   public int size() {
-      // TODO: Implement this
-      
-   }
+    @Override
+    public int size() {
+        return currentIndex + 1;
+    }
 
-   @Override
-   public void clear() {
-      // TODO: Implement this
-      
-   }
+    @Override
+    public void clear() {
+        currentIndex = -1;
+    }
 
-   @Override
-   public boolean isEmpty() {
-      // TODO: Implement this
-      
-   }
+    @Override
+    public boolean isEmpty() {
+        return currentIndex == -1;
+    }
 
-   @Override
-   public String toString() {
-      StringBuilder builder = new StringBuilder("[");
-      for (var index = 0; index <= currentIndex; index++) {
-         builder.append(itemArray[index].toString());
-         if (index < currentIndex) {
-            builder.append(", ");
-         }
-      }
-      builder.append("]");
-      return builder.toString();
-   }
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("[");
+        for (int index = 0; index <= currentIndex; index++) {
+            builder.append(itemArray[index].toString());
+            if (index < currentIndex) {
+                builder.append(", ");
+            }
+        }
+        builder.append("]");
+        return builder.toString();
+    }
+
+    private void ensureCapacity() {
+        if (size() == capacity()) {
+            int newCapacity = capacity * 2 + 1;
+            E[] newArray = (E[]) new Object[newCapacity];
+            System.arraycopy(itemArray, 0, newArray, 0, itemArray.length);
+            itemArray = newArray;
+            capacity = newCapacity;
+        }
+    }
 }
