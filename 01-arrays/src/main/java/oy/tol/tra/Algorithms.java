291e66c9f6dd05b6ef6ed46735fa2a179340b61b
Raining <127360832+Raining9966@users.noreply.github.com>
2024-04-01 11:32:56 +0800

    Update Algorithms.java

--- 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
+++ 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
@@ -1,13 +1,19 @@
 package oy.tol.tra;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
-public class Grades<T extends Comparable<T>> {
+public class Grades<T extends Number & Comparable<T>> {
 
     private T[] grades;
+    private Comparator<T> comparator;
 
-    public Grades(T[] grades) {
+    public Grades(T[] grades, Comparator<T> comparator) {
+        if (grades == null || comparator == null) {
+            throw new IllegalArgumentException("Input array and comparator cannot be null.");
+        }
         this.grades = grades.clone();
+        this.comparator = comparator;
     }
 
     public void reverse() {
@@ -19,10 +25,28 @@ public class Grades<T extends Comparable<T>> {
     }
 
     public void sort() {
-        Arrays.sort(grades);
+        Arrays.sort(grades, comparator);
     }
 
     public T[] getArray() {
         return grades.clone();
+    }
+
+    public double calculateAverage() {
+        double sum = 0;
+        for (T grade : grades) {
+            sum += grade.doubleValue();
+        }
+        return sum / grades.length;
+    }
+
+    public T findHighestGrade() {
+        T highest = grades[0];
+        for (T grade : grades) {
+            if (grade.compareTo(highest) > 0) {
+                highest = grade;
+            }
+        }
+        return highest;
     }
 }
