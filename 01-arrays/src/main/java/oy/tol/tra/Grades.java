bbf2b6b0a71f6d32a8042b731d0780bdc945c89b
Raining <127360832+Raining9966@users.noreply.github.com>
2024-04-15 14:00:49 +0800

    Update Grades.java

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -1,48 +1,40 @@
 package oy.tol.tra;
 
-import java.util.Arrays;
-
 public class Grades<T extends Comparable<T>> {
 
-    private T[] grades;
+   private T[] grades = null;
+   public Grades(T[] grades) {
+      this.grades = grades.clone();
+   }
+    
+   public void reverse() {
+      int start = 0;
+      int end = grades.length - 1;
 
-    public Grades(T[] grades) {
-        if (grades == null) {
-            throw new IllegalArgumentException("Grades array cannot be null.");
-        }
-        this.grades = grades.clone();
-    }
+      while (start < end) {
+         T temp = grades[start];
+         grades[start] = grades[end];
+         grades[end] = temp;
+         start++;
+         end--;
+      }
+   }
 
-    protected void reverse() {
-        int start = 0;
-        int end = grades.length - 1;
-
-        while (start < end) {
-            T temp = grades[start];
-            grades[start] = grades[end];
-            grades[end] = temp;
-            start++;
-            end--;
-        }
-    }
-
-    protected void sort() {
-        boolean swapped;
-        do {
-            swapped = false;
-            for (int i = 1; i < grades.length; i++) {
-                if (grades[i - 1].compareTo(grades[i]) > 0) {
-                    T temp = grades[i - 1];
-                    grades[i - 1] = grades[i];
-                    grades[i] = temp;
-                    swapped = true;
-                }
+   public void sort() {
+      boolean swapped=true;
+      while (swapped==true){
+         swapped = false;
+         for (int i = 1; i < grades.length; i++) {
+            if (grades[i - 1].compareTo(grades[i]) > 0) {
+               T temp = grades[i - 1];
+               grades[i - 1] = grades[i];
+               grades[i] = temp;
+               swapped = true;
             }
-        } while (swapped);
-    }
-
-    public T[] getArray() {
-        return grades.clone();
-    }
+         }
+      }
+   }
+   public T[] getArray() {
+      return grades.clone();
+   }
 }
-
